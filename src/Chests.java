da100ba4205e9bb1790fbfd83cfab3e7c056837a
Nick049 <np3100@students.inghamisd.org>
2022-10-05 12:43:12 -0400

    stuff and things (chests and skeletons)

---
+++ src/Chests.java
@@ -0,0 +1,25 @@
+/**
+ * Created by np3100 on 10/5/2022.
+ */
+public class Chests {
+}
+
+    Tier I Encounter Chest
+Intro: “You encounter an ever-so-slightly rusty chest, made from oak wood and lined with iron”
+        Drop: “You found (1-3) Health Potions in the rusty chest”
+
+        Tier II Encounter Chest
+        Intro: “You come across a chest made from aged oak wood and lined with steel”
+        Drop: “You found (1-4) Health Potions, (0-2) Berserker Potions, and (0-1) Mana Potions in the aged chest”
+
+        Tier III Encounter/Tier I Boss Chest
+        Intro: “You find a mysterious chest made from some form of carved stone. It seems very ornate in nature”
+        Drop: “You found (2-5) Health Potions, (1-3) Berserker Potions, and (1-2) Mana Potions in the ornate stone chest.”
+
+        Tier II Boss Chest
+        Intro: “You come across an ornate-looking chest made from rich oak wood and lined with gleaming steel. A piece of topaz adorns the front of the container.”
+        Drop: “You found (2-6) Health Potions, (2-4) Berserker Potions, and (2-3) Mana Potions in the gleaming chest.”
+        
+        Tier III Boss Chest
+        Intro: “You come across an ornate-looking chest made from rich oak wood and lined with gleaming steel. A piece of amethyst adorns the front of the container.”
+        Drop: “You found (2-7) Health Potions, (2-5) Berserker Potions, and (2-4) Mana Potions in the gleaming chest.”
---
+++ src/SkeletonEnemies1.java
@@ -0,0 +1,50 @@
+/**
+ * Created by np3100 on 10/5/2022.
+ */
+public class SkeletonEnemies1 {
+}
+
+Skeleton
+        Intro: “A Skeleton pulls itself from its resting place”
+        Attack 1: “The Skeleton swings it’s rusted blade towards (Char) and deals _ damage”
+        Attack 2: “In a fury, the Skeleton brings down it’s ancient sword on (Char), dealing _ damage”
+        Death: “The Skeleton collapses into a pile of (finally) lifeless bones”
+        Drop: “The Skeleton dropped (0-1) Health Potion”
+
+        Skull Raider
+        Intro: “A Skull Raider approaches, its weapon drawn”
+        Attack 1: “The Skull Raider brings down it’s ancient axe at (Char), dealing _ damage”
+        Attack 2: “In it’s rampage, the Skull Raider swings it’s axe wildly toward (Char), dealing _ damage”
+        Death: “The Skull Raider falls apart, collapsing to the ground in a pile of lifeless bones”
+        Drop: “The Skull Raider dropped (0-2) Health Potion and (0-1) Berserker Potion”
+
+        Skull Knight
+        Intro: “An armored Skull Knight approaches, its weapon drawn”
+        Attack 1: “The Skull Knight brings down its mighty blade at (Char), dealing _ damage”
+        Attack 2: “An enraged Skull Knight swings it’s blade wildly at (Char), dealing _ damage”
+        Death: “The Skull Knight’s armor clangs to the ground as it collapses into a pile of lifeless bones”
+        Drop: “The Skull Knight dropped (1-3) Health Potion and (0-2) Berserker Potion”
+
+        Skull Mage
+        Intro: “A robed Skull Mage appears, it’s scepter at the ready”
+        Attack 1: “The Skull Mage fires off a bolt of white fire at (Char), dealing _ damage”
+        Attack 2: “The Skull Mage summons a Spectral Skull that explodes near (Char) and deals _ damage”
+        Death: “The Skull Mage collapses into a pile of demonic ash and bone”
+        Drop: “The Skull Mage dropped (1-2) Health Potion and (1-2) Mana Potion
+
+        Skull Cavalry
+        Intro: “A Skull Cavalryman on skeletal horseback rides into battle”
+        Attack 1: “The Skull Cavalry’s lance jabs at (Char), dealing _ damage”
+        Attack 2: “The Skull Cavalry’s horse stomps (Char), dealing _ damage”
+        Death: “The Skull Cavalry collapses to the ground, both horse and rider no more than a pile of bones.”
+        Drop: “The Skull Cavalry dropped (1-4) Health Potion and (0-3) Berserker Potion”
+
+        Lord of The 13th Grave
+        Intro: As dark fog fills the room, an ominous roar fills the party’s ears. The dragging of a ball and chain signifies the danger, and with it, the Lord of The 13th Grave reveals itself!
+        Attack 1: “The Lord of The 13th Grave swings its giant mace at (Char), dealing _ damage”
+        Attack 2: “The Lord of The 13th Grave smashes it’s massive flail at (Char), dealing _ damage”
+        Attack 3: “The Lord of The 13th Grave calls forth a surge of spectral white fire to burn (Char), dealing _ damage”
+        Attack 4: “The Lord of The 13th Grave hurls a boulder at (Char), dealing _ damage”
+        Ability 1: “The Lord of The 13th Grave calls forth (1-4) of its skeletal disciples.”
+        Death: “The Lord of The 13th Grave collapses as the sounds of the souls trapped within it cry out in freedom. It is dead, and you lived to see another day.”
+        Drop: “The Lord of The 13th Grave left behind (1-6) Health Potion, (1-4) Berserker Potion, (1-4) Mana Potion, and (0-1) Potion of 1,000 Souls
